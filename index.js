38ea97a150311b3755327739432acc0409994942
Abdullah A. Noman <43316038+noman-nahil@users.noreply.github.com>
2021-10-01 14:34:06 +0600

    Adding express and testing with postman

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
-const express = require("express")
+const express = require("express");
+const bodyParser = require("body-parser");
 
-const app = express()
 
+
+const app = express();
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.json());
 app.get("/hello", (req, res) => res.send("Hello"))
+app.post("/hello", (req, res) => res.send(`Post ${req.body.name}`))
 
 app.listen(8000, () => {
     console.log('App listening on port 8000!');
