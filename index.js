3a47ab0147c3568a19c01c2e89e017fcf7c355ad
Abdullah A. Noman <43316038+noman-nahil@users.noreply.github.com>
2021-10-01 14:39:46 +0600

    Route Parameters in Express

--- index.js
+++ index.js
@@ -9,6 +9,8 @@ app.use(express.json());
 app.get("/hello", (req, res) => res.send("Hello"))
 app.post("/hello", (req, res) => res.send(`Post ${req.body.name}`))
 
+app.get("/hello/:name", (req, res) => res.send(`Hello ${req.params.name}`))
+
 app.listen(8000, () => {
     console.log('App listening on port 8000!');
 });
