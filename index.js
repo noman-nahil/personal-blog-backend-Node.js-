a8818a15ed4e91e3f30d735e16883babb9536101
Abdullah A. Noman <43316038+noman-nahil@users.noreply.github.com>
2021-10-02 00:32:21 +0600

    Adding MongoDB to express

--- index.js
+++ index.js
@@ -1,20 +1,28 @@
 const express = require("express");
 const bodyParser = require("body-parser");
-const articleInfo = {
-    'learn-react': {
-        comments: [],
-    }, 'learn-node': {
-        comments: [],
-    }, 'my-thoughts-on-learning-react': {
-        comments: [],
-    },
-}
-
+const { MongoClient } = require('mongodb');
 
 const app = express();
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.json());
-app.get("/hello", (req, res) => res.send("Hello !!!"))
+
+app.get("/api/articles/:name", async (req, res) => {
+    try {
+        const articleName = req.params.name;
+        const client = await MongoClient.connect('mongodb://localhost:27017', {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        });
+        const db = client.db("blog");
+        const articleInfo = await db.collection('articles').findOne({ name: articleName })
+        res.status(200).json(articleInfo)
+        client.close();
+    }
+    catch {
+        res.status(500).json({ message: "Error connecting to db", error })
+    }
+
+})
 // app.post("/hello", (req, res) => res.send(`Post ${req.body.name}`))
 
 // app.get("/hello/:name", (req, res) => res.send(`Hello ${req.params.name}`))
